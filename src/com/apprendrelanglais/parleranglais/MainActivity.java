25f14b540548699a9d7e49a08eb7111600bf0b52
enigmasck <timothyasanders@gmail.com>
2014-04-03 16:56:34 -0400

    almost there for the drawer navi

--- src/com/apprendrelanglais/parleranglais/MainActivity.java
+++ src/com/apprendrelanglais/parleranglais/MainActivity.java
@@ -1,11 +1,19 @@
 package com.apprendrelanglais.parleranglais;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.*;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 //using this for layouts
@@ -19,6 +27,9 @@ public class MainActivity extends Activity {
 	private String[] phraseDirectionTitles;
 	private DrawerLayout phraseDrawerLayout;
 	private ListView phraseListView;
+	
+	private CharSequence phraseDrawerTitle;
+	private CharSequence phrasetitle; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +48,7 @@ public class MainActivity extends Activity {
         												phraseMainTitles));
         
         phraseListView.setOnItemClickListener(new DrawerItemClickListener());
-        
-        
-       
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,10 +58,57 @@ public class MainActivity extends Activity {
     }
     
 	private void selectItem(int position){
-		Fragment fragment = new TitleFragement;
+		Fragment fragment = new TitleFragment();
+		Bundle args = new Bundle();
+		args.putInt(TitleFragment.ARG_TITLE_NUMBER, position);
+		fragment.setArguments(args);
 		
+	    FragmentManager fragmentManager = getFragmentManager();
+	    fragmentManager.beginTransaction()
+	                   .replace(R.id.content_frame, fragment)
+	                   .commit();
+	    
+	    phraseListView.setItemChecked(position, true);
+	    setTitle(phraseMainTitles[position]);
+	    phraseDrawerLayout.closeDrawer(phraseListView);
 		
 	}
+	
+	public static class TitleFragment extends Fragment{
+	    public static final String ARG_TITLE_NUMBER = "title number";
+
+	    public TitleFragment() {
+	        // Empty constructor required for fragment subclasses
+	    }
+
+	    @Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	        View rootView = inflater.inflate(R.layout.fragment_title, container, false);
+	        int i = getArguments().getInt(ARG_TITLE_NUMBER);
+	        String planet = getResources().getStringArray(R.array.phraseMainTitles)[i];
+
+	        int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
+	                        "drawable", getActivity().getPackageName());
+	        ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+	        getActivity().setTitle(planet);
+	        return rootView;
+	    }
+	}
+	
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    
+    public void setTitle(CharSequence title){
+    	newTitle = title;
+    	
+    }
+
+
 
     
 }
